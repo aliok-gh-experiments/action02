c94a442fe5d1d8082cf93024d4ddf79c377b7618
Ali Ok <aliok@redhat.com>
2024-04-28 00:30:20 +0300

    Testing stuff

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ export async function run(): Promise<void> {
     core.info("TESTING")
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', "AAAA " + new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
